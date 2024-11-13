71261c88e16b1ffcecda5aaf97571011b55d4874
Wern7ck <you@example.com>
2024-11-13 09:53:43 -0300

    ApiRestful introdução

--- src/routes/index.js
+++ src/routes/index.js
@@ -1,9 +1,16 @@
 const { Router } = require("express");
 
 const usersRouter = require("./users.routes");
+const sessionsRouter = require("./sessions.routes")
+const moviesRouter = require("./movies.routes");
+const tagsRouter = require("./Tags.routes")
+
 
 const routes = Router();
 
-routes.use("/users", usersRouter)
+routes.use("/users", usersRouter);
+routes.use("/sessions", sessionsRouter);
+routes.use("/movies", moviesRouter);
+routes.use("/tags", tagsRouter);
 
 module.exports = routes;
